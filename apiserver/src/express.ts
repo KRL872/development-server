aa1fdf5ae444530cb6e49fec40c57fc15f597bd6
Kristopher Lancaster <KR.Lancaster@hotmail.com>
2025-02-14 11:18:16 +0000

    fix   bootstrapped the main code so it can use async, wait Add Feature  urlEncoding processor Add Feature cookie processos Add Feature Authentication Module Add Feature Routes Authentication Add Feature Routes (Users)

--- apiserver/src/express.ts
+++ apiserver/src/express.ts
@@ -1,27 +1,65 @@
 import express, { Express, Request, Response } from "express"
-import cors from "cors"
+import cookieParser from "cookie-parser"
+import cors, { CorsOptions } from "cors"
 import dotenv from "dotenv"
+
+import authenticateInitialize from "./authenticate-passport-sessions"
+import * as models from "./models";
+import authRoutes from "./authRoutes"
+import userRoutes from "./userRoutes"
+
 dotenv.config();
 
-const port = process.env.PORT
-const app: Express = express();
+const egg = async () => {
+    const PORT = process.env.PORT;
+    const CORS_ALLOWED_ORIGIN = process.env.CORS_ALLOWED_ORIGIN;
+    const app: Express = express();
+
+    // Enable JSON parsing
+    app.use(express.json())
+
+    // Enable URL encoded String Parsing
+    app.use(express.urlencoded({ extended: true }));
+
+    // Enable Parsing of Cookies
+    app.use(cookieParser())
+
+    // Enable Cors
+    const corsConfig: CorsOptions = {
+        origin: CORS_ALLOWED_ORIGIN,
+        credentials: true
+    }
+    const corsMiddleWare = cors(corsConfig)
+    app.use(corsMiddleWare);
+
+    // Initialize Authentication Strategy
+    const authConfig: models.AuthenticationConfig = {
+        database: "API",
+        provider: "MONGOCLIENT",
+        store: "MONGOSTORE",  //not implemented Yet
+        sessionCollection: "SESSIONS",
+        userCollection: "USERS"
+    }
+    const authenticate = await authenticateInitialize(authConfig, app)
+
+    // Public Routes Need for Authentication
+    app.use("/auth", authRoutes(authenticate))
+
+    // Private Routes Need to Be Authenticated
+    app.use("/users", userRoutes())
+
+    // Backup To Catch All the Extra Requests
+    app.all("*", (req: Request, res: Response) => {
+        res.sendStatus(404)
+    })
 
 
-app.use(express.json())
-
-app.use(cors({
-    origin: "http://bigrobot.ca:2000",
-    credentials: true
-}));
 
 
-app.get("/auth/status", (req: Request, res: Response) => {
-    console.log("Api Request")
-    res.status(200).json({ message: "Hello From Api" })
-})
+    app.listen(PORT, () => {
+        console.log(`Api Server Running on Port: ${PORT}`)
+    })
 
-app.listen(port, () => {
-    console.log(`Api Server Running on Port: ${port}`)
-})
-
+}
+egg();
 
