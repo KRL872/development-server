87e2a0865a0cce5e047b93a31f2fc0997f1b47e3
Kristopher Lancaster <KR.Lancaster@hotmail.com>
2025-02-17 06:59:26 +0000

    add feature new api command to fetprofile

--- apiclient/src/apifunctions/functions.tsx
+++ apiclient/src/apifunctions/functions.tsx
@@ -1,3 +1,5 @@
+import * as models from "../models/models";
+
 export const fetchStatus = async(): Promise<boolean> => {
     try {
         const response = await fetch("http://bigrobot.ca:3000/auth/status",{
@@ -6,7 +8,7 @@ export const fetchStatus = async(): Promise<boolean> => {
         if (response.status === 200) return true;
         return false;
     } catch (error) {
-
+        console.error("Error fetching status:", error);
         return false;
         
     }
@@ -18,6 +20,36 @@ export const fetchLogout = async (): Promise<boolean> => {
       });
       return response.status === 200;
     } catch (error) {
+      console.error("Error logging out:", error);
       return false;
     }
+  }
+
+  export const fetchMyProfile = async (): Promise<models.User | null> => {
+    try {
+      const response = await fetch("http://bigrobot.ca:3000/profile", {
+        credentials: "include",
+      });
+      if (response.status === 200) {
+        return await response.json() as models.User;
+      }
+    } catch (error) {
+      console.error("Error fetching profile:", error);
+      return null
+    }
+    return null;
+  }
+  export const fetchProfile = async (name:string): Promise<models.User | null> => {
+    try {
+      const response = await fetch(`http://bigrobot.ca:3000/profile/${name}`, {
+        credentials: "include",
+      });
+      if (response.status === 200) {
+        return await response.json() as models.User;
+      }
+    } catch (error) {
+      console.error("Error fetching profile:", error);
+      return null
+    }
+    return null;
   }
